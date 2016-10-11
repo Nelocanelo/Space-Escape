86d51ea16874a71ff3039fa0d642a690be729be9
Nel <al228478@uji.es>
2016-10-11 16:52:13 +0200

    0.0.1.1
    TE ODIO CILINDRO REBELDE :@

---
+++ Assets/ToMove.cs
@@ -0,0 +1,35 @@
+﻿// MoveTo.cs
+using UnityEngine;
+using System.Collections;
+
+public class ToMove : MonoBehaviour
+{
+
+    public Transform goal;
+    public Transform goal2;
+    NavMeshAgent agent;
+    bool Waypointreached = false;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    void Update()
+    {
+        Debug.Log(agent.remainingDistance);
+        if (Waypointreached == false)
+        {
+            agent.destination = goal.position;
+            if (agent.remainingDistance < 0.1)
+            {
+                Waypointreached = true;
+            }
+        }
+        else
+        {
+            agent.destination = goal2.position;
+        }
+    }
+}
